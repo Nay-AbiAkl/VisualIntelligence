cfaa479df256524b8427519a205673b9df717b9c
Nay-AbiAkl <nayabiakl@gmail.com>
2023-05-08 15:33:01 +0200

    changing the network

--- ran_policy.py
+++ ran_policy.py
@@ -103,7 +103,7 @@ class Policy(abc.ABC):
         return True
 
     @property
-    def num_recurrent_layers(self) -> int:
+    def num_hidden_channels(self) -> int:
         return 0
 
     def forward(self, *x):
@@ -213,8 +213,8 @@ class NetPolicy(nn.Module, Policy):
         return True
 
     @property
-    def num_recurrent_layers(self) -> int:
-        return self.net.num_recurrent_layers
+    def num_hidden_channels(self) -> int:
+        return self.net.num_hidden_channels
 
     def forward(self, *x):
         raise NotImplementedError
@@ -355,7 +355,7 @@ class PointNavBaselinePolicy(NetPolicy):
 
 
 # we will not use this base class for the network, replace it with the GCN network
-"""class Net(nn.Module, metaclass=abc.ABCMeta):
+class Net(nn.Module, metaclass=abc.ABCMeta):
     @abc.abstractmethod
     def forward(self, observations, rnn_hidden_states, prev_actions, masks):
         pass
@@ -367,19 +367,13 @@ class PointNavBaselinePolicy(NetPolicy):
 
     @property
     @abc.abstractmethod
-    def num_recurrent_layers(self):
+    def num_hidden_channels(self):
         pass
 
     @property
     @abc.abstractmethod
     def is_blind(self):
         pass
-
-    @property
-    @abc.abstractmethod
-    def perception_embedding_size(self) -> int:
-        pass
-"""
 
 
 # Our own defined network
@@ -420,68 +414,157 @@ class GCN(nn.Module):
         return x
 
 
-"""class PointNavBaselineNet(Net):
-    rNetwork which passes the input image through CNN and concatenates
-    goal vector with CNN's output and passes that through RNN.
-    
+class RingAttractorNetworkGraph:
+    """Graph representing the ring attractor network"""
+
+    def __init__(self, nb_of_nodes: int) -> None:
+        # Note that final center node is of idx nb_of_nodes-1
+        self.nb_of_nodes = nb_of_nodes
+        self.ran_graph = nx.MultiDiGraph()
+
+        # Add nb_of_nodes nodes
+        nodes = range(0, self.nb_of_nodes - 1)
+        self.ran_graph.add_nodes_from(nodes)
+
+        # Add bidirectional connections for the first nb_of_nodes-1 nodes
+        for i in range(0, self.nb_of_nodes - 2):
+            self.ran_graph.add_edge(i, i + 1)
+            self.ran_graph.add_edge(i + 1, i)
+
+        # Add connections between idx nb_of_nodes-2 and 0
+
+        self.ran_graph.add_edge(self.nb_of_nodes - 2, 0)
+        self.ran_graph.add_edge(0, self.nb_of_nodes - 2)
+
+        # Add unidirectional connections for the last node
+        for i in range(0, self.nb_of_nodes - 1):
+            self.ran_graph.add_edge(self.nb_of_nodes - 1, i)
+
+        # add edge features
+        edge_feat = self.get_edge_features()
+        for idx, (_, _, e) in enumerate(self.ran_graph.edges(data=True)):
+            e["feature"] = edge_feat[idx]
+
+    # Function to prepare the node features
+    def get_node_features(
+        self,
+        image_encoding: torch.Tensor,
+        nb_connections: int = 2,  # TODO: CHANGE TO OPTIONAL
+        odometry: Optional[torch.Tensor] = None,
+    ):
+        """_summary_
+
+        Args:
+            nb_nodes (int): total nb of nodes (including the center node), odd number
+            image_encoding (torch.Tensor): embeddings of the image
+            node_pos_encoding (_type_): circular positional encoding
+            nb_connections (Optional[int], optional): nb of connections of each node.
+            Defaults to 2.
+            odometry (Optional[torch.Tensor], optional): odom data Defaults to None.
+        """
+        positions = self.distribute_nodes_on_circle(radius=1)
+
+        nb_connections_tensor = torch.tensor(nb_connections).unsqueeze(0)
+        nb_connections_tensor = nb_connections_tensor.repeat(self.nb_of_nodes - 1, 1)
+        nb_connections_tensor = torch.cat(
+            (nb_connections_tensor, torch.tensor([self.nb_of_nodes - 1]).unsqueeze(0)),
+            0,
+        )
+
+        image_encoding = image_encoding.repeat(self.nb_of_nodes, 1)
+
+        feature_vector = torch.concat(
+            (image_encoding, nb_connections_tensor, positions), 1
+        )
+
+        if odometry is not None:
+            odometry = odometry.repeat(self.nb_of_nodes, 1)
+            feature_vector = torch.concat((feature_vector, odometry), 1)
+
+        return feature_vector
+
+    # Function to prepare the edge features
+    def get_edge_features(self):
+        """_summary_
+
+        Args:
+            nb_nodes (int): total nb of nodes (including the center node), odd number
+        """
+        # Define the edge features tensor with shape (2 * nb_nodes + nb_nodes, 1)
+        # first 2 * nb_nodes are for the connections between the nodes, should be 1
+        # last nb_nodes are for the connections between the new node and the existing
+        # nodes, should be -1
+        edge_features = torch.ones(
+            (2 * (self.nb_of_nodes - 1) + (self.nb_of_nodes - 1), 1)
+        )
+        edge_features[2 * (self.nb_of_nodes - 1) :, :] = -1
+
+        return edge_features
+
+    def get_edge_index(self):
+        # create edge index from
+        adj = nx.to_scipy_sparse_array(self.ran_graph).tocoo()
+        row = torch.from_numpy(adj.row.astype(np.int64)).to(torch.long)
+        col = torch.from_numpy(adj.col.astype(np.int64)).to(torch.long)
+        edge_index = torch.stack([row, col], dim=0)
+
+        return edge_index
+
+    def distribute_nodes_on_circle(self, radius=1):
+        angles = np.linspace(
+            0, 2 * np.pi, self.nb_of_nodes - 1, endpoint=False
+        )  # Divide the circle into equal angles
+        x = radius * np.cos(angles)  # Calculate x-coordinates using cosine function
+        y = radius * np.sin(angles)  # Calculate y-coordinates using sine function
+        positions = np.column_stack(
+            (x, y)
+        )  # Stack x and y coordinates as column vectors
+
+        positions = np.vstack((positions, np.array([0, 0])))
+        return torch.tensor(positions)
+
+
+class GCNPointNavBaselineNet(Net):
+    r"""Graph Convolutional Network for PointNav baseline"""
 
     def __init__(
         self,
         observation_space: spaces.Dict,
-        hidden_size: int,
+        state_encoder_input_channels: int,
+        state_encoder_hidden_channels: int,
+        state_encoder_out_channels: int,
+        nb_of_nodes: int,
     ):
         super().__init__()
 
-        if IntegratedPointGoalGPSAndCompassSensor.cls_uuid in observation_space.spaces:
-            self._n_input_goal = observation_space.spaces[
-                IntegratedPointGoalGPSAndCompassSensor.cls_uuid
-            ].shape[0]
-        elif PointGoalSensor.cls_uuid in observation_space.spaces:
-            self._n_input_goal = observation_space.spaces[
-                PointGoalSensor.cls_uuid
-            ].shape[0]
-        elif ImageGoalSensor.cls_uuid in observation_space.spaces:
-            goal_observation_space = spaces.Dict(
-                {"rgb": observation_space.spaces[ImageGoalSensor.cls_uuid]}
-            )
-            self.goal_visual_encoder = SimpleCNN(goal_observation_space, hidden_size)
-            self._n_input_goal = hidden_size
+        self.state_encoder_input_channels = state_encoder_input_channels
+        self.state_encoder_hidden_channels = state_encoder_hidden_channels
+        self.state_encoder_out_channels = state_encoder_out_channels
+        self.nb_of_nodes = nb_of_nodes
 
-        self._hidden_size = hidden_size
+        self.ring_network = RingAttractorNetworkGraph(self.nb_of_nodes)
 
-        # replace this with our own encoding of the image
-        self.visual_encoder = SimpleCNN(observation_space, hidden_size)
-
-        self.state_encoder = build_rnn_state_encoder(
-            (0 if self.is_blind else self._hidden_size) + self._n_input_goal,
-            self._hidden_size,
+        self.state_encoder = GCN(
+            state_encoder_input_channels,
+            state_encoder_hidden_channels,
+            state_encoder_out_channels,
         )
 
         self.train()
 
     @property
     def output_size(self):
-        return self._hidden_size
+        return (
+            self.state_encoder_out_channels
+        )  # state encoder hidden channels is another option
 
     @property
-    def is_blind(self):
-        return self.visual_encoder.is_blind
-
-    @property
-    def num_recurrent_layers(self):
-        return self.state_encoder.num_recurrent_layers
-
-    @property
-    def perception_embedding_size(self):
-        return self._hidden_size
+    def num_hidden_channels(self):
+        return self.state_encoder_hidden_channels
 
     def forward(
         self,
         observations,
-        rnn_hidden_states,
-        prev_actions,
-        masks,
-        rnn_build_seq_info: Optional[Dict[str, torch.Tensor]] = None,
     ):
         aux_loss_state = {}
         if IntegratedPointGoalGPSAndCompassSensor.cls_uuid in observations:
@@ -501,97 +584,24 @@ class GCN(nn.Module):
             x = [perception_embed] + x
             aux_loss_state["perception_embed"] = perception_embed
 
-        x_out = torch.cat(x, dim=1)
-        x_out, rnn_hidden_states = self.state_encoder(
-            x_out, rnn_hidden_states, masks, rnn_build_seq_info
+        # x_out = torch.cat(x, dim=1)
+
+        image_encoding = perception_embed
+
+        node_feat = self.ring_network.get_node_features(
+            image_encoding, 2, odometry=None
         )
-        aux_loss_state["rnn_output"] = x_out
 
-        return x_out, rnn_hidden_states, aux_loss_state
-"""
+        # Set the node and edge features in the graph object
+        for i, feat in enumerate(node_feat):
+            self.ring_network.ran_graph.nodes[i]["feature"] = feat
 
+        # Generate the input features and edge index tensor for the model
+        x = torch.tensor(node_feat, dtype=torch.float)
+        edge_index = self.ring_network.get_edge_index()
+        edge_index = torch.tensor(edge_index, dtype=torch.long)
 
-class GCNPointNavBaselineNet(GCN):
-    r"""Network which passes the input image through CNN and concatenates
-    goal vector with CNN's output and passes that through RNN.
-    """
+        # Compute the graph embedding using the GCN model
+        embedding = self.state_encoder(x, edge_index)
 
-    def __init__(
-        self,
-        observation_space: spaces.Dict,
-        hidden_size: int,
-    ):
-        super().__init__()
-
-        if IntegratedPointGoalGPSAndCompassSensor.cls_uuid in observation_space.spaces:
-            self._n_input_goal = observation_space.spaces[
-                IntegratedPointGoalGPSAndCompassSensor.cls_uuid
-            ].shape[0]
-        elif PointGoalSensor.cls_uuid in observation_space.spaces:
-            self._n_input_goal = observation_space.spaces[
-                PointGoalSensor.cls_uuid
-            ].shape[0]
-        elif ImageGoalSensor.cls_uuid in observation_space.spaces:
-            goal_observation_space = spaces.Dict(
-                {"rgb": observation_space.spaces[ImageGoalSensor.cls_uuid]}
-            )
-            self.goal_visual_encoder = SimpleCNN(goal_observation_space, hidden_size)
-            self._n_input_goal = hidden_size
-
-        self._hidden_size = hidden_size
-
-        # replace this with our own encoding of the image
-        """self.visual_encoder = SimpleCNN(observation_space, hidden_size)
-
-        self.state_encoder = build_rnn_state_encoder(
-            (0 if self.is_blind else self._hidden_size) + self._n_input_goal,
-            self._hidden_size,
-        )"""
-
-        self.train()
-
-    @property
-    def output_size(self):
-        return self._hidden_size
-
-    @property
-    def num_recurrent_layers(self):
-        return self.state_encoder.num_recurrent_layers
-
-    @property
-    def perception_embedding_size(self):
-        return self._hidden_size
-
-    def forward(
-        self,
-        observations,
-        rnn_hidden_states,
-        prev_actions,
-        masks,
-        rnn_build_seq_info: Optional[Dict[str, torch.Tensor]] = None,
-    ):
-        aux_loss_state = {}
-        if IntegratedPointGoalGPSAndCompassSensor.cls_uuid in observations:
-            target_encoding = observations[
-                IntegratedPointGoalGPSAndCompassSensor.cls_uuid
-            ]
-        elif PointGoalSensor.cls_uuid in observations:
-            target_encoding = observations[PointGoalSensor.cls_uuid]
-        elif ImageGoalSensor.cls_uuid in observations:
-            image_goal = observations[ImageGoalSensor.cls_uuid]
-            target_encoding = self.goal_visual_encoder({"rgb": image_goal})
-
-        x = [target_encoding]
-
-        if not self.is_blind:
-            perception_embed = self.visual_encoder(observations)
-            x = [perception_embed] + x
-            aux_loss_state["perception_embed"] = perception_embed
-
-        x_out = torch.cat(x, dim=1)
-        x_out, rnn_hidden_states = self.state_encoder(
-            x_out, rnn_hidden_states, masks, rnn_build_seq_info
-        )
-        aux_loss_state["rnn_output"] = x_out
-
-        return x_out, rnn_hidden_states, aux_loss_state
+        return embedding, aux_loss_state
