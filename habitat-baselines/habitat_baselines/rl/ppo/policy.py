619b6e4a292ebf97f963177bc8972a3e4aa93e29
Nay-AbiAkl <nayabiakl@gmail.com>
2023-05-27 10:43:55 +0200

    add graph embedding to rnn input

--- habitat-baselines/habitat_baselines/rl/ppo/policy.py
+++ habitat-baselines/habitat_baselines/rl/ppo/policy.py
@@ -23,6 +23,21 @@ from transformers import ViTFeatureExtractor, ViTMAEForPreTraining
 if TYPE_CHECKING:
     from omegaconf import DictConfig
 
+import matplotlib.pyplot as plt
+import networkx as nx
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.utils.data as utils
+from torch import nn, optim
+from torch.utils.data.sampler import SubsetRandomSampler
+from torch_geometric.data import Data
+from torch_geometric.loader import DataLoader
+from torch_geometric.nn import GCNConv, global_mean_pool
+from torch_geometric_temporal.nn.recurrent import GConvGRU
+from transformers import ViTConfig, ViTFeatureExtractor, ViTMAEForPreTraining
+
 
 class vitmae:
     def __init__(self):
@@ -341,17 +356,24 @@ class PointNavBaselineNet(Net):
             goal_observation_space = spaces.Dict(
                 {"rgb": observation_space.spaces[ImageGoalSensor.cls_uuid]}
             )
-            # self.goal_visual_encoder = SimpleCNN(goal_observation_space, hidden_size)
-            self.goal_visual_encoder = vitmae()
+            self.goal_visual_encoder = SimpleCNN(goal_observation_space, hidden_size)
+            # self.goal_visual_encoder = vitmae()
             self._n_input_goal = hidden_size
 
         self._hidden_size = hidden_size
 
-        # self.visual_encoder = SimpleCNN(observation_space, hidden_size)
-        self.visual_encoder = vitmae()
+        self.visual_encoder = SimpleCNN(observation_space, hidden_size)
+
+        self.graph_encoder = GCN(
+            517,
+            512,
+            hidden_size,
+        )
 
         self.state_encoder = build_rnn_state_encoder(
-            (0 if self.is_blind else self._hidden_size) + self._n_input_goal,
+            (0 if self.is_blind else self._hidden_size)
+            + self._n_input_goal
+            + hidden_size,
             self._hidden_size,
         )
 
@@ -400,9 +422,199 @@ class PointNavBaselineNet(Net):
             aux_loss_state["perception_embed"] = perception_embed
 
         x_out = torch.cat(x, dim=1)
+
+        # Add the graph network
+        batch_size = x_out.shape[0]
+        train_dataset = []
+
+        for i in range(batch_size):
+            device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+            ring_network = RingAttractorNetworkGraph(512)  # (self.nb_of_nodes)
+
+            # get image encoding for the current image
+            node_feat = ring_network.get_node_features(x_out[i], 2, odometry=None)
+
+            # Set the node and edge features in the graph object
+            for i, feat in enumerate(node_feat):
+                ring_network.ran_graph.nodes[i]["feature"] = feat
+
+            # Generate the input features and edge index tensor for the model
+            x = torch.tensor(node_feat, dtype=torch.float)
+
+            edge_index = ring_network.get_edge_index()
+            edge_index = torch.tensor(edge_index, dtype=torch.long)
+
+            data = Data(x=x, edge_index=edge_index, edge_attr=ring_network.edge_feat)
+
+            train_dataset.append(data)
+
+        train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+
+        # Compute the graph embedding using the GCN model
+        # We will only have one iteration since the dataset is always of shape
+        # batch size
+        embedding = torch.zeros((batch_size, self._hidden_size))
+        for data in train_loader:
+            device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+            data.x = data.x.to(device)
+            data.edge_index = data.edge_index.to(device)
+            data.batch = data.batch.to(device)
+            embedding = self.state_encoder(data.x, data.edge_index, data.batch)
+
+        # embedding is of shape (batch_size, hidden_size of CNN)
+        print("embedding shape: ", embedding.shape)
+        print("x_out shape: ", x_out.shape)
+
+        x_out = torch.cat((x_out, embedding), dim=1)
+
+        # Add the graph embedding to the input
         x_out, rnn_hidden_states = self.state_encoder(
             x_out, rnn_hidden_states, masks, rnn_build_seq_info
         )
         aux_loss_state["rnn_output"] = x_out
 
         return x_out, rnn_hidden_states, aux_loss_state
+
+
+#################### Added for the graph network ####################
+# Our own defined network
+class GCN(nn.Module):
+    def __init__(self, num_features, hidden_channels, out_channels):
+        super(GCN, self).__init__()
+        self.conv1 = GConvGRU(num_features, hidden_channels, K=2)
+        self.conv2 = GConvGRU(hidden_channels, out_channels, K=2)
+
+    def forward(self, x, edge_index, batch):
+        # Apply the first GCN layer
+        x = self.conv1(x, edge_index)
+        x = F.relu(x)
+        x = F.dropout(x, p=0.5, training=self.training)
+
+        # Apply the second GCN layer
+        x = self.conv2(x, edge_index)
+        x = F.relu(x)
+        x = F.dropout(x, p=0.5, training=self.training)
+
+        # Global pooling to obtain the graph embedding
+        x = global_mean_pool(x, batch)
+
+        return x
+
+
+class RingAttractorNetworkGraph:
+    """Graph representing the ring attractor network"""
+
+    def __init__(self, nb_of_nodes: int) -> None:
+        # Note that final center node is of idx nb_of_nodes-1
+
+        self.nb_of_nodes = nb_of_nodes
+        self.ran_graph = nx.MultiDiGraph()
+
+        # Add nb_of_nodes nodes
+        nodes = range(0, self.nb_of_nodes - 1)
+        self.ran_graph.add_nodes_from(nodes)
+
+        # Add bidirectional connections for the first nb_of_nodes-1 nodes
+        for i in range(0, self.nb_of_nodes - 2):
+            self.ran_graph.add_edge(i, i + 1)
+            self.ran_graph.add_edge(i + 1, i)
+
+        # Add connections between idx nb_of_nodes-2 and 0
+
+        self.ran_graph.add_edge(self.nb_of_nodes - 2, 0)
+        self.ran_graph.add_edge(0, self.nb_of_nodes - 2)
+
+        # Add unidirectional connections for the last node
+        for i in range(0, self.nb_of_nodes - 1):
+            self.ran_graph.add_edge(self.nb_of_nodes - 1, i)
+
+        # add edge features
+        self.edge_feat = self.get_edge_features()
+        for idx, (_, _, e) in enumerate(self.ran_graph.edges(data=True)):
+            e["feature"] = self.edge_feat[idx]
+
+    # Function to prepare the node features
+    def get_node_features(
+        self,
+        image_encoding: torch.Tensor,
+        nb_connections: int = 2,  # TODO: CHANGE TO OPTIONAL
+        odometry: Optional[torch.Tensor] = None,
+    ):
+        """_summary_
+
+        Args:
+            nb_nodes (int): total nb of nodes (including the center node), odd number
+            image_encoding (torch.Tensor): embeddings of the image
+            node_pos_encoding (_type_): circular positional encoding
+            nb_connections (Optional[int], optional): nb of connections of each node.
+            Defaults to 2.
+            odometry (Optional[torch.Tensor], optional): odom data Defaults to None.
+        """
+        positions = self.distribute_nodes_on_circle(radius=1)
+
+        nb_connections_tensor = torch.tensor(nb_connections).unsqueeze(0)
+        nb_connections_tensor = nb_connections_tensor.repeat(self.nb_of_nodes - 1, 1)
+        nb_connections_tensor = torch.cat(
+            (
+                nb_connections_tensor,
+                torch.tensor([self.nb_of_nodes - 1]).unsqueeze(0),
+            ),
+            0,
+        )
+        # print("before repeat", image_encoding.shape)
+        image_encoding = image_encoding.repeat(self.nb_of_nodes, 1)
+
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        image_encoding = image_encoding.to(device)
+        nb_connections_tensor = nb_connections_tensor.to(device)
+        positions = positions.to(device)
+
+        feature_vector = torch.concat(
+            (image_encoding, nb_connections_tensor, positions), 1
+        )
+        # print("before repeat", image_encoding.shape)
+        if odometry is not None:
+            odometry = odometry.repeat(self.nb_of_nodes, 1)
+            feature_vector = torch.concat((feature_vector, odometry), 1)
+
+        return feature_vector
+
+    # Function to prepare the edge features
+    def get_edge_features(self):
+        """_summary_
+
+        Args:
+            nb_nodes (int): total nb of nodes (including the center node), odd number
+        """
+        # Define the edge features tensor with shape (2 * nb_nodes + nb_nodes, 1)
+        # first 2 * nb_nodes are for the connections between the nodes, should be 1
+        # last nb_nodes are for the connections between the new node and the existing
+        # nodes, should be -1
+        edge_features = torch.ones(
+            (2 * (self.nb_of_nodes - 1) + (self.nb_of_nodes - 1), 1)
+        )
+        edge_features[2 * (self.nb_of_nodes - 1) :, :] = -1
+
+        return edge_features
+
+    def get_edge_index(self):
+        # create edge index from
+        adj = nx.to_scipy_sparse_array(self.ran_graph).tocoo()
+        row = torch.from_numpy(adj.row.astype(np.int64)).to(torch.long)
+        col = torch.from_numpy(adj.col.astype(np.int64)).to(torch.long)
+        edge_index = torch.stack([row, col], dim=0)
+
+        return edge_index
+
+    def distribute_nodes_on_circle(self, radius=1):
+        angles = np.linspace(
+            0, 2 * np.pi, self.nb_of_nodes - 1, endpoint=False
+        )  # Divide the circle into equal angles
+        x = radius * np.cos(angles)  # Calculate x-coordinates using cosine function
+        y = radius * np.sin(angles)  # Calculate y-coordinates using sine function
+        positions = np.column_stack(
+            (x, y)
+        )  # Stack x and y coordinates as column vectors
+
+        positions = np.vstack((positions, np.array([0, 0])))
+        return torch.tensor(positions)
